b2ec7a6368a51b63ba7a59b8286a11e4177964ea
Lenso43 <lstossel43@gmail.com>
2023-01-06 18:32:05 -0800

    finished assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -2,8 +2,36 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+  static void Main(string[] args)
+  {
+    // Console.WriteLine("Hello Prep2 World!");
+    Console.Write("Enter a number for a grade: ");
+    string gradeValue = Console.ReadLine();
+
+    int x = int.Parse(gradeValue);
+    // int a = 90;
+    // int b = 80;
+    // int c = 70;
+    if (x >= 90)
     {
-        Console.WriteLine("Hello Prep2 World!");
+      Console.WriteLine("you have an A");
     }
+    else if (x >= 80 && x < 90)
+    {
+      Console.WriteLine("you have an B");
+    }
+    else if (x >= 70 && x < 80)
+    {
+      Console.WriteLine("you have an C");
+    }
+    else if (x >= 60 && x < 70)
+    {
+      Console.WriteLine("you have an D");
+    }
+    else if (x <= 60)
+    {
+      Console.WriteLine("you have an F");
+    }
+
+  }
 }
