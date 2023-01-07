233aaa2adb315fef06f02eefe7c9630356260d0d
Lenso43 <lstossel43@gmail.com>
2023-01-06 17:44:24 -0800

    completed first assignment

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -2,8 +2,16 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep1 World!");
-    }
+  static void Main(string[] args)
+  {
+    Console.Write("What is your First name? ");
+    string firstname = Console.ReadLine();
+
+    Console.Write("What is your Last name? ");
+    string lastname = Console.ReadLine();
+
+    Console.WriteLine($"Your name is {lastname}, {firstname} {lastname}");
+  }
+
+
 }
